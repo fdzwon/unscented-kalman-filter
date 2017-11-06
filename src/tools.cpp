d06c8f5a1ff7d500998cf0eb1bcf2fa0fb9d6711
Frank Dzwonkowski <fdzwon@gmail.com>
2017-11-06 05:30:54 -0800

    update readme.txt and comments

--- src/tools.cpp
+++ src/tools.cpp
@@ -12,7 +12,6 @@ Tools::~Tools() {}
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
   /**
-  TODO:
     * Calculate the RMSE here.
   */
 	VectorXd rmse(4);
